014919586a1e27b033429f2770b30a115d525b6f
diegos9307 <diegos9307@gmail.com>
2022-05-25 15:26:59 -0500

    set up inicial del mongoose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-const app = require("express")();
 require("dotenv").config();
 
-require("../config/dbconfig");
+const app = require("express")();
+const path = require("path");
+
+require("./config/dbConfig");
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Estoy funcionando</h1>`);
+});
 
 app.listen(
   process.env.PORT,
