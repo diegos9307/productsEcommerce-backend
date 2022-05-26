c976265ae4783f1da386ef0af5c7bcd6e1e160b9
diegos9307 <diegos9307@gmail.com>
2022-05-25 23:17:38 -0500

    Config inicial de las router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import "dotenv/config";
 import "./config/dbConfig.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.json({ ok: true });
-});
+app.use(express.json());
+app.use("/", authRouter);
 
 app.listen(PORT, console.log(`running in port ${PORT}`));
