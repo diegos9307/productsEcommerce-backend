3a18fa85b02de0338bde7d43e1b855ef70829c6c
diegos9307 <diegos9307@gmail.com>
2022-05-26 10:08:44 -0500

    validaciones iniciales usando express-validations

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,42 @@
 import express from "express";
 import { login, register } from "../controllers/auth.controller.js";
+import pkg from "express-validator";
+import { validationResultExpress } from "../middlewares/validationResultExpress.js";
+const { body, check, custom } = pkg;
 const router = express.Router();
 
-router.post("/register", register);
+router.post(
+  "/register",
+  [
+    body("username", "Formato username es incorrecto").custom((value) => {
+      return value.match(/^[A-Za-z ]+$/);
+    }),
+    body("email", "Formato de email incorrecto").trim().isEmail().normalizeEmail(),
+    body(
+      "password",
+      "La contraseña debe tener mínimo 8 carácteres, 1 letra mayúscula, 1 símbolo y 1 número"
+    )
+      .trim()
+      .isStrongPassword(),
+    body("terms", "Formato username es incorrecto").isBoolean()
+  ],
+  validationResultExpress,
+  register
+);
 
-router.post("/login", login);
+router.post(
+  "/login",
+  [
+    body("email", "Formato de email incorrecto").trim().isEmail().normalizeEmail(),
+    body(
+      "password",
+      "La contraseña debe tener mínimo 8 carácteres, 1 letra mayúscula, 1 símbolo y 1 número"
+    )
+      .trim()
+      .isStrongPassword()
+  ],
+  validationResultExpress,
+  login
+);
 
 export default router;
